807343fa759f93989f47ca33992778798e068c0e
abinns2011 <abinns2011@my.fit.edu>
2015-07-04 17:21:35 -0400

    Upgradez

--- Core/src/bdmds/core/backend/U.java
+++ Core/src/bdmds/core/backend/U.java
@@ -1,9 +1,20 @@
 package bdmds.core.backend;
 
+import java.io.Closeable;
+import java.io.IOException;
 import java.io.PrintStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.InvalidParameterException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.LinkedHashMap;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -14,6 +25,56 @@ import java.util.Scanner;
  */
 public class U
 {
+	/**
+	 * Calls the specified method with the specified parameters, checking to
+	 * make sure passed arguments match required ones before attempting an
+	 * invocation.
+	 *
+	 * @param <T>
+	 * @param method
+	 *            is the method which is being attempted to be invoked.
+	 * @param input
+	 *            is a var-args (any number of options, or an array) for the
+	 *            parameters.
+	 * @throws InvalidParameterException
+	 *             if the passed options don't match with the arguments required
+	 *             by the method.
+	 */
+
+	@SuppressWarnings("unchecked")
+	public static <T> T carefulCall(Method method, Object target, Object... params) throws InvalidParameterException
+	{
+		Object res = null;
+		// int i = 0;
+		// for (Class<?> c : method.getParameterTypes())
+		// if (!c.equals(U.getUnBoxedType(params[i++].getClass())))
+		// U.w("Input item " + params[i - 1].getClass().getName() +
+		// " may be incompatible with " + c.getName() + " for method " +
+		// method.getName());
+
+		try
+		{
+			res = method.invoke(target, params);
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e)
+		{
+			U.e("Error, could not properly call method " + method.getName(), e);
+		}
+		return (T) res;
+	}
+
+	/**
+	 * Casting wrapper, centralizes the "unchecked" warning needs.
+	 *
+	 * @param cur
+	 *            the object to cast
+	 * @return the object casted to the specified type.
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T> T cleanCast(Object cur)
+	{
+		return (T) cur;
+	}
+
 	/**
 	 * Simple confirmation function, returns a true or false in response to the
 	 * passed message.
@@ -113,9 +174,76 @@ public class U
 	 * @param E
 	 *            the exception to also print.
 	 */
+	public static void e(String in, Exception E)
+	{
+		U.printWithTag(in + " - " + E, "ERROR");
+		E.printStackTrace();
+	}
+
+	/**
+	 * Prints the specified string and exception, for error logging.
+	 *
+	 * @param in
+	 *            the string to print
+	 * @param E
+	 *            the exception to also print.
+	 */
 	public static void e(String in, Throwable E)
 	{
 		U.printWithTag(in + " - " + E, "ERROR");
+	}
+
+	public static String getTimestamp()
+	{
+		return U.timeStampFormatter.format(Calendar.getInstance().getTime());
+	}
+
+	/**
+	 * <p>
+	 * Returns the unboxed version of this class if this is boxed.
+	 * </p>
+	 * <p>
+	 * Example: given Double.class this will return double.class
+	 * </p>
+	 * <p>
+	 * Returns input if the passed type isn't a wrapper type.
+	 *
+	 * @param input
+	 *            the type to unbox
+	 * @return the unboxed type
+	 */
+	public static Class<?> getUnBoxedType(Class<?> input)
+	{
+		if (U.WRAPPER_TYPES.containsKey(input))
+			return U.WRAPPER_TYPES.get(input);
+		return input;
+	}
+
+	private static LinkedHashMap<Class<?>, Class<?>> getWrapperTypes()
+	{
+		LinkedHashMap<Class<?>, Class<?>> ret = new LinkedHashMap<Class<?>, Class<?>>();
+		ret.put(Boolean.class, boolean.class);
+		ret.put(Character.class, char.class);
+		ret.put(Byte.class, byte.class);
+		ret.put(Short.class, short.class);
+		ret.put(Integer.class, int.class);
+		ret.put(Long.class, long.class);
+		ret.put(Float.class, float.class);
+		ret.put(Double.class, double.class);
+		ret.put(Void.class, void.class);
+		return ret;
+	}
+
+	/**
+	 * Checks if the passed class is a wrapper type (Double, Integer, etc)
+	 *
+	 * @param clazz
+	 *            the class to check
+	 * @return true of this is a wrapper class, false otherwise.
+	 */
+	public static boolean isWrapperType(Class<?> clazz)
+	{
+		return U.WRAPPER_TYPES.containsKey(clazz);
 	}
 
 	public static void p(double[] arr)
@@ -216,7 +344,7 @@ public class U
 	private static void printWithTag(String in, String tag)
 	{
 		StringBuilder res = new StringBuilder();
-		res.append(U.formatter.format(Calendar.getInstance().getTime()));
+		res.append(U.outputFormatter.format(Calendar.getInstance().getTime()));
 		res.append("[");
 		res.append(tag);
 		res.append("] ");
@@ -280,13 +408,90 @@ public class U
 		return result;
 	}
 
-	private static PrintStream		output;
+	/**
+	 * Tries to close the passed stream, if it is not null. If it encounters an
+	 * exception, it logs it and continues.
+	 *
+	 * @param message
+	 *            the message to log on IO exception
+	 * @param stream
+	 *            the stream to try and close
+	 */
 
-	private static int				debugging	= 0;
+	public static void tryCloseStream(String message, Closeable stream)
+	{
+		if (stream != null)
+			try
+			{
+				stream.close();
+			} catch (IOException e)
+			{
+				U.e(message, e);
+			}
+	}
 
-	private static SimpleDateFormat	formatter	= new SimpleDateFormat("HH:mm:ss.SSS");
+	/**
+	 * Prints the specified warning
+	 *
+	 * @param input
+	 *            some warning text
+	 */
+	public static void w(String input)
+	{
+		U.printWithTag(input, "WARNING");
+	}
 
-	private static Scanner			input;
+	/**
+	 * Given a file and contents, attempts to write the entire string provided
+	 * to the file. By default, uses the UTF_8 charset. If the file does not
+	 * exist, it creates it. If the file exists, it overwrites the contents,
+	 * truncating prior to any writing.
+	 *
+	 * @param path
+	 *            the path of the file to write to
+	 * @param contents
+	 *            the data to be written to the file
+	 */
+	public static void writeToFile(String path, String contents)
+	{
+		U.writeToFile(path, contents, StandardCharsets.UTF_8);
+	}
+
+	/**
+	 * Given a file and contents, and Charset, attempts to write the entire
+	 * string provided to the file using the specified encoding. If the file
+	 * does not exist, it creates it. If the file exists, it overwrites the
+	 * contents, truncating prior to any writing.
+	 *
+	 * @param path
+	 *            the path of the file to write to
+	 * @param contents
+	 *            the data to be written to the file
+	 * @param encoding
+	 *            the string encoding method to use
+	 */
+	public static void writeToFile(String path, String contents, Charset encoding)
+	{
+		try
+		{
+			Files.write(Paths.get(path), contents.getBytes(encoding));
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	private static PrintStream								output;
+
+	private static int										debugging			= 0;
+
+	private static SimpleDateFormat							outputFormatter		= new SimpleDateFormat("HH:mm:ss.SSS");
+
+	private static SimpleDateFormat							timeStampFormatter	= new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss.SSS");
+
+	private static Scanner									input;
+
+	private static Random									rand;
 
 	/**
 	 * Static constructor, used to initialize output to the system.out
@@ -297,5 +502,23 @@ public class U
 	{
 		U.output = System.out;
 		U.input = new Scanner(System.in);
+		U.rand = new Random();
+	}
+
+	private static final LinkedHashMap<Class<?>, Class<?>>	WRAPPER_TYPES		= U.getWrapperTypes();
+
+	/**
+	 * Gives a random float between the specified min and max.
+	 *
+	 * @param min
+	 *            the minimum value for the result
+	 * @param max
+	 *            the maximum value for the result
+	 * @return a random number bewteen the specified min and max
+	 */
+
+	public float rand(float min, float max)
+	{
+		return U.rand.nextFloat() * (max - min) + min;
 	}
 }
